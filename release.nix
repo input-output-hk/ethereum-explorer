c959d8602b427c1eb4d0dc7b638f0b6d95336927
Kris Jenkins <krisajenkins@gmail.com>
2018-09-06 13:04:28 +0100

    Adding a versions file to the hydra build targets.

--- release.nix
+++ release.nix
@@ -1,8 +1,30 @@
 { nixpkgs ? <nixpkgs>
 , ethExplorerSrc ? ./.
 }:
-with import nixpkgs {};
-{
+let
+  listVersions = derivations: pkgs.stdenv.mkDerivation {
+    name = "versions";
+    src = ./.;
+    buildInputs = [ pkgs.git ];
+    installPhase =
+      builtins.concatStringsSep "\n"
+        (
+          [ "mkdir $out" ]
+          ++
+          (builtins.map
+            (derivation: ''
+                (
+                  cd ${derivation.src}
+                  SHA=`git rev-parse HEAD || echo UNKNOWN`
+                  printf "%40s %s\n" $SHA ${derivation.name}
+                ) >> $out/versions.txt
+             '')
+             derivations)
+        );
+  };
+in with import nixpkgs {}; {
+  versions = listVersions [ ethExplorer ];
+
   ethExplorer = callPackage ./. {
     inherit ethExplorerSrc;
   };
